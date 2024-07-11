c58707531d3efce73d64c8e27f1d670e0baf3b62
Nikhil Gurung <nikkhilgurung@gmail.com>
2024-07-11 13:31:08 +0530

    commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
-import { Routes } from '@angular/router';
+// import {NgModule} from "@angular/core";
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {SellAuthComponent} from "./sell-auth/sell-auth.component";
+import {NgModule} from "@angular/core";
 
-export const routes: Routes = [];
+const routes: Routes = [{
+  path:'',
+  component:HomeComponent
+},
+  {
+    path:'sell-auth',
+    component:SellAuthComponent
+  }]
+
+@NgModule({
+  imports:[RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+
+export class AppRoutes{}
